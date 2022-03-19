573ed514005a2e0bd18783344764638e256c1d49
danijeljw-rpc <danijel@repasscloud.com>
2022-03-19 11:22:29 +1100

    starting countries list

---
+++ Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace WebApi.Entities;
+
+public enum Country
+{
+    [EnumMember(Value = "Not Selected")]Not_Selected,
+    [EnumMember(Value = "Australia")]Australia,
+    [EnumMember(Value = "Canada")]Canada,
+    [EnumMember(Value = "Croatia")]Croatia,
+    [EnumMember(Value = "France")]France,
+    [EnumMember(Value = "Hong Kong")]Hong_Kong,
+    [EnumMember(Value = "Macau")]Macau,
+    [EnumMember(Value = "New Zealand")]New_Zealand,
+    [EnumMember(Value = "Singapore")]Singapore,
+    [EnumMember(Value = "Ukraine")]Ukraine,
+    [EnumMember(Value = "United Arab Emitates")]United_Arab_Emirates,
+    [EnumMember(Value = "United Kingdom")]United_Kingdom,
+    [EnumMember(Value = "United States")]United_States_of_America
+}
