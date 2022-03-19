c7c394282290ead299e302f4a36c41bce292a2ba
danijeljw-rpc <danijel@repasscloud.com>
2022-03-19 12:26:01 +1100

    helper script

---
+++ install_frameworks.ps1
@@ -0,0 +1,11 @@
+# EF Core Packages
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore
+
+# pgsql Packages
+dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+
+# Snake Case package 🐍
+dotnet add package EFCore.NamingConventions
