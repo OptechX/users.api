b34108a6acd03fbe5d8ae383a7bc216dfa740bdf
danijeljw-rpc <danijel@repasscloud.com>
2022-03-27 02:32:46 +1100

    reformat account response #7

--- Models/Accounts/AccountResponse.cs
+++ Models/Accounts/AccountResponse.cs
@@ -2,31 +2,28 @@ namespace WebApi.Models.Accounts;
 
 public class AccountResponse
 {
-    // public int Id { get; set; }
-    // public string? Company { get; set; }
-    // public string? TaxID { get; set; }
-    // public string FirstName { get; set; }
-    // public string LastName { get; set; }
-    // public string Email { get; set; }
-    // public string Role { get; set; }
-    // public DateTime Created { get; set; }
-    // public DateTime? Updated { get; set; }
-    // public bool IsVerified { get; set; }
     public int Id { get; set; }
-    public string? Company { get; set; }
-    public string? TaxID { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+    public string UUID { get; set; }
+    public string Company { get; set; }
+    public string TaxID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Email { get; set; }
-    public string? Phone { get; set; }
-    public string? Address { get; set; }
-    public string? City { get; set; }
-    public string? State { get; set; }
-    public string? PostCodeZip { get; set; }
-    public string? Country { get; set; }
-    public string? Role { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string PostCodeZip { get; set; }
+    public string Country { get; set; }
+    public string UserIcon { get; set; }
     public DateTime Created { get; set; }
-    public DateTime? Updated { get; set; }
-    public bool? IsVip { get; set; }
+    public int ServiceLevelResponseMinutes { get; set; }
+    public string EnterpriseAgreement { get; set; }
+    public string MSEnterpriseAgreementNumber { get; set; }
     public string BillingType { get; set; }
+    public string AccountTier { get; set; }
+    public int ImagesRemaining { get; set; }
+    public int AppLockerStorageAvailable { get; set; }
+    public int AppLockerStorageUsed { get; set; }
+    public bool AccountNotifications { get; set; }
 }
