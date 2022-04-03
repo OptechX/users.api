6fc0863eef053e3e3d92754193ab6ec4fd328b3b
danijeljw-rpc <danijel@repasscloud.com>
2022-04-03 22:53:08 +1000

    typo

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -495,7 +495,7 @@ public class AccountService : IAccountService
                     <tr>
                       <td style=""font-family: sans-serif; font-size: 14px; vertical-align: top;"" valign=""top"">
                         <p style=""font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;"">Hi,</p>
-                        <p style=""font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;"">You're almost ready to start enjoying building effortless SOE images with OptechX. Simply click the big blue botton below to verify your email address.</p>
+                        <p style=""font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;"">You're almost ready to start enjoying building effortless SOE images with OptechX. Simply click the button below to verify your email address.</p>
                         <table role=""presentation"" border=""0"" cellpadding=""0"" cellspacing=""0"" class=""btn btn-primary"" style=""border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; box-sizing: border-box; width: 100%;"" width=""100%"">
                           <tbody>
                             <tr>
